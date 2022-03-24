283eb3617bae9e738078dc8ad3cbdcdadee02104
Milaen13 <liudmila.engberg@gmail.com>
2022-03-24 17:53:22 +0100

    второй commit

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,21 +1,23 @@
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
 
-        String host = "127.0.0.1";
-        int port = 8888 ;
+        String host = "localhost";
+        int port = 8089;
 
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-             String resp = in.readLine();
-             System.out.println(resp);
-        }
-        catch (IOException e) {
+            out.println("Mila\n" + "Host: localhost\n\n\n");
+            String resp = in.readLine();
+            System.out.println(resp);
+            
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,31 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
+        public static void main (String[] args) throws IOException {
 
-        System.out.println("Запуск сервера");
-        int port =8888;
+            ServerSocket serverSocket = new ServerSocket(8089); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-
-        while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket clientSocket = serverSocket.accept(); // ждем подключения
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                 System.out.println("New connection accepted" + clientSocket.getPort());
-
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-            }
-            catch(IOException e){
-                e.printStackTrace();
+            System.out.println("New connection accepted");
+            final String name = in.readLine();
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
             }
         }
-    }
-}
